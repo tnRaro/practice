5efb3e055507c986e3d88d5c5938b0aebbc6d56e
tnRaro <admin@tnraro.com>
2018-03-19 16:10:08 +0900

    cpp; 01+

--- 2018/cpp/01/02.cpp
+++ 2018/cpp/01/02.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
+template<typename T, typename U>
+auto add(T t, U u) -> decltype(t + u) {
+    return t + u;
+}
+
 int main() {
     auto a = 1 + 2;
     auto b = "Hi";
     auto c = { 1, 2 };
-    auto increase = [](int x) { return x + 1; };
+    auto sum = add(5, 0.3f);
 
     for (auto i : c) {
         std::cout << i << std::endl;
     }
-    
+
     return 0;
 }
